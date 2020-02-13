b124f8242a1723b16c16068cb9a1b896355969e1
ebresser <ebresser@student.42sp.org.br>
2020-02-13 18:32:02 -0300

    Day 25_Debbuging strncmp + memmove

---
+++ ft_memmove.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebresser <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/13 09:16:04 by ebresser          #+#    #+#             */
+/*   Updated: 2020/02/13 18:08:35 by ebresser         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static unsigned char	*cpy_inv(unsigned char *a, unsigned char *b, size_t len)
+{
+	size_t i;
+
+	i = 0;
+	while (i < len)
+		a[i] = b[len - 1 - i];
+	return (a);
+}
+void					*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char	*psrc;
+	unsigned char	*pdst;
+
+	pdst = (unsigned char*)dst;
+	psrc = (unsigned char*)src;
+	if(dst == src)
+		return (dst);
+	else
+	{
+		if(dst > src)
+			cpy_inv(pdst, psrc, len);
+		else
+			ft_memcpy(pdst, psrc,len);
+		return (dst);
+	}
+}
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,25 +6,38 @@
 /*   By: ebresser <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/31 11:16:12 by ebresser          #+#    #+#             */
-/*   Updated: 2020/02/12 20:13:18 by ebresser         ###   ########.fr       */
+/*   Updated: 2020/02/13 16:28:01 by ebresser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+static int	diff(const char *a, const char *b, int i)
+{
+	unsigned char ua;
+	unsigned char ub;
+
+	ua = (unsigned char)a[i];
+	ub = (unsigned char)b[i];
+	return (ua - ub);
+}
+int			ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
+	int		compare;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
+	compare = 1;
+	while (compare)
 	{
-		if (s1[i] != s2[i])
-			return ((int)((unsigned char)s1[i] - (unsigned char)s2[i]));
-		i++;
+		if (i == n)
+			return (0);
+		else if (s1[i] == '\0' || s2[i] == '\0')
+			compare = 0;
+		else if (s1[i] != s2[i])
+			compare = 0;
+		else
+			i++;
 	}
-	if (s1[i] == '\0' || s2[i] == '\0')
-		return ((int)((unsigned char)s1[i] - (unsigned char)s2[i]));
-	else if (i == n)
-		return (0);
+	return (diff(s1, s2, i));
 }

169dd29244c79fd8c720e906261bd8d92b5f6c8d
ebresser <ebresser@student.42sp.org.br>
2020-02-15 19:10:53 -0300

    Day 27-Debbuging atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,32 +6,60 @@
 /*   By: ebresser <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 16:47:01 by ebresser          #+#    #+#             */
-/*   Updated: 2020/02/15 17:51:35 by ebresser         ###   ########.fr       */
+/*   Updated: 2020/02/15 19:00:16 by ebresser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	convert(char *s, int signal)
+{
+	size_t	i;
+	size_t	pot;
+	int		n;
+
+	i = 0;
+	pot = 1;
+	n = 0;
+	while (s[i] > 47 && s[i] < 58)
+	{
+			i++;
+			if (i > 0)
+				pot = pot*10;
+	}
+	while (i >= 0)
+	{
+		n = n + s[i] * pot;
+		i--;
+		pot = pot/10;
+	}
+	if (signal < 0)
+		n = n * (-1);
+	return (n);
+}
+
 int	ft_atoi(const char *str)
 {
-	unsigned char *p;
+	char *p;
 	int i;
 	int signal;
 
 	signal = 0;
-	p = (unsigned char *)nptr;
-	while (!isalpha(p[i]))
+	i = 0;
+	p = (char *)str;
+	while (p[i] == ' ')
+		i++;
+	if (p[i] == '-')
 	{
-		if (p[i] == ' ' && !signal)
-			i++;
-		else if (p[i] == '-')
-			signal--;
-		else if (p[i] == '+')
-			signal++;			
-		else if (isdigit(p[i]))
-
-
-
-
-
+		signal--;
+		i++;
+	}
+	else if (p[i] == '+')
+	{
+		signal++;			
+		i++;
+	}
+	else if (p[i] > 47 && p[i] < 58) 
+		return (convert(p + i, signal));
+	return (0);
 }

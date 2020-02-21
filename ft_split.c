a71c5a2d30d12b4d29a7681be41b321c29ddfe45
ebresser <ebresser@student.42sp.org.br>
2020-02-20 21:32:18 -0300

    Day 33_Debbuging split

--- ft_split.c
+++ ft_split.c
@@ -6,38 +6,39 @@
 /*   By: ebresser <ebresser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 12:02:23 by ebresser          #+#    #+#             */
-/*   Updated: 2020/02/20 14:58:36 by ebresser         ###   ########.fr       */
+/*   Updated: 2020/02/20 21:27:06 by ebresser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-static char	**fill(char **array, size_t len, char	*st, char cut)
+static char	**fill(char **array, size_t len, char *st, char cut)
 {
 	size_t	i;
 	size_t	j;
-	
+	char	stop;
+
 	i = 0;
-	if (!len)
-		return (NULL);
-	while (i++ < len - 1)
+	if (len == 1)
+		array[0] = NULL;
+	while (i < len - 1)
 	{
+		stop = cut;
 		j = 0;
 		if (i == len - 2)
-			cut = '\0';
-		while (st[j++] != cut) 
-			//j++;
-		array[i] = (char *)malloc( (j + 1) *sizeof(char));
+			stop = '\0';
+		while (st[j] != stop) 
+			j++;
+		array[i] = (char *)malloc( (j + 1) * sizeof(char));
 		if (!array[i])
 			return (NULL);
-		ft_strlcpy(array[i], st, j + 1); 
-		st = st + j + 1; 
+		ft_strlcpy(array[i], st, j + 1); //ver nulo
+		st = st + j; 
 		while (*st == cut)
 			st++;
-		//i++;
-		array[len - 1] = NULL;
+		i++;
 	}
+	array[len - 1] = NULL;
 	return (array);
 }
 
@@ -54,7 +55,10 @@ char	**ft_split(char const *s, char c)
 	sm[0] = c;
 	sm[1] = '\0';
 	ps = ft_strtrim(s, sm);
-	size = 2;
+	if (ps)
+		size = 2;
+	else
+		size = 1;
 	i = 0;
 	while (ps[i])
 	{
@@ -66,10 +70,12 @@ char	**ft_split(char const *s, char c)
 		}
 		i++;
 	}
-		//i++;
 	splited = (char **)malloc(size * sizeof(char *));
 	if (!splited)
 		return (NULL);
-	//funcao
-	return (fill(splited, size, ps, c));
+	if (*ps == '\0')
+		splited[0] = NULL;
+	else
+		splited = fill(splited, size, ps, c);
+	return (splited);
 }

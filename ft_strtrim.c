26d726f80ff9f77b39b7ddbc196e65f90d0e6927
lilangbr <ebresser@MacBook-Pro-de-Lais-Lang.local>
2020-03-21 20:10:21 -0300

    Day 61_1

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,41 +12,39 @@
 
 #include "libft.h"
 
-static int		isin(char c, char const *set)
+static int		is_in(char c, char const *set)
 {
 	if (!*set)
 		return (0);
 	else if (c == *set)
 		return (1);
 	else
-		return (isin(c, ++set));
+		return (is_in(c, ++set));
 }
 
 static int		marker(const char *s, const char *set, int begin)
 {
 	int			i;
-	size_t		go;
 	int			iter;
 	size_t		s_len;
 
 	s_len = ft_strlen(s);
 	i = 0;
 	iter = 1;
-	go = 1;
 	if (!begin)
 	{
 		i = s_len - 1;
 		iter = -1;
 	}
-	while (s_len && go)
+	while (s_len)
 	{
-		if (isin(s[i], set) && s[i] != '\0' && i > -1)
+		if (is_in(s[i], set) && s[i] != '\0' && i > -1)
 		{
 			i = i + iter;
 			s_len--;
 		}
 		else
-			go = 0;
+			break;
 	}
 	return (i);
 }
@@ -56,19 +54,15 @@ char			*ft_strtrim(char const *s1, char const *set)
 	int		b;
 	int		e;
 	char	*s2;
-	char	*s;
-	int		len;
 
 	if (!s1 || !set)
 		return (NULL);
-	s = (char*)s1;
 	b = marker(s1, set, 1);
-	e = marker(s1, set, 0);
-	len = e - b + 1;
+	e = marker(s1, set, 0) + 1;
 	if (b > e)
-		len = 0;
-	if (!(s2 = (char *)malloc((len + 1) * sizeof(char))))
+        b = e;
+	if (!(s2 = (char *)malloc((e - b + 1) * sizeof(char))))
 		return (NULL);
-	ft_strlcpy(s2, &s[b], len + 1);
+	ft_strlcpy(s2, &s1[b], e - b + 1);
 	return (s2);
 }

de6244f916699be461d413c09e63bfe44b12942b
lilangbr <ebresser@MacBook-Pro-de-Lais-Lang.local>
2020-05-04 14:20:48 -0300

    Calloc - if 0X0. Error fixed

--- ft_calloc.c
+++ ft_calloc.c
@@ -16,11 +16,6 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void *ptr;
 
-	if (count == 0 || size == 0)
-	{
-		count = 1;
-		size = 1;
-	}
 	if (!(ptr = malloc(count * size)))
 		return (NULL);
 	ft_bzero(ptr, (count * size));
